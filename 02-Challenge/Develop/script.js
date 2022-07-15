adac3ac103cb3a887ddf95f77e34ab11d037bd49
Nerms <Nerms@Nermins-MacBook-Pro.local>
2022-07-14 21:57:11 -0500

    changed local storage code to save to browser.

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -8,9 +8,6 @@ var saveEvent = document.getElementById('saveBtn')
 var comment = document.getElementById('time-block')
 
 
-// $("#note").val(localStorage.getItem ("comment"));
-// localStorage.setItem($(".time-block").val());
-
 
 var checkTime = function () {
     var currentTime = moment().format('H');
@@ -40,3 +37,88 @@ var checkTime = function () {
 }
 // check the time every half hour
 setInterval(checkTime(), (1000 * 60) * 30);
+
+
+//local storage
+
+// save button on click 
+//function event and claim variable 
+
+// Save calendar item to local storage when save button is clicked
+$(".saveBtn").on("click", function(event) {
+  var calenderEvent = event.target.parentElement.parentElement.previousElementSibling.children[0].value;
+  localStorage.setItem(event.target.parentElement.attributes[0].value, calenderEvent);
+});
+
+// Function to populate calendar events if they are in local storage
+$(document).ready(function () {
+    if (localStorage["saveNine"] !== null && localStorage["saveNine"] !== undefined) {
+      var calenderEventNine = $("<p>" + localStorage["saveNine"] + "</p>");
+      $("#nine").append(calenderEventNine[0].innerText);
+    }
+    else {
+      ("");
+    }});
+
+$(document).ready(function () {
+    if (localStorage["saveTen"] !== null && localStorage["saveTen"] !== undefined) {
+      var calenderEventTen = $("<p>" + localStorage["saveTen"] + "</p>");
+      $("#ten").append(calenderEventTen[0].innerText);
+    }
+    else {
+      ("");
+    }});
+
+$(document).ready(function () {
+    if (localStorage["saveEleven"] !== null && localStorage["saveEleven"] !== undefined) {
+      var calenderEventEleven = $("<p>" + localStorage["saveEleven"] + "</p>");
+      $("#eleven").append(calenderEventEleven[0].innerText);
+    }
+    else {
+      ("");
+    }});
+
+$(document).ready(function () {
+    if (localStorage["saveTwelve"] !== null && localStorage["saveTwelve"] !== undefined) {
+      var calenderEventTwelve = $("<p>" + localStorage["saveTwelve"] + "</p>");
+      $("#twelve").append(calenderEventTwelve[0].innerText);
+    }
+    else {
+      ("");
+    }});
+
+$(document).ready(function () {
+    if (localStorage["saveThirteen"] !== null && localStorage["saveThirteen"] !== undefined) {
+      var calenderEventThirteen = $("<p>" + localStorage["saveThirteen"] + "</p>");
+      $("#thirteen").append(calenderEventThirteen[0].innerText);
+    }
+    else {
+      ("");
+    }});
+
+$(document).ready(function () {
+    if (localStorage["saveFourteen"] !== null && localStorage["saveFourteen"] !== undefined) {
+      var calenderEventFourteen = $("<p>" + localStorage["saveFourteen"] + "</p>");
+      $("#fourteen").append(calenderEventFourteen[0].innerText);
+    }
+    else {
+      ("");
+    }});
+
+$(document).ready(function () {
+    if (localStorage["saveFifteen"] !== null && localStorage["saveFifteen"] !== undefined) {
+      var calenderEventFifteen = $("<p>" + localStorage["saveFifteen"] + "</p>");
+      $("#fifteen").append(calenderEventFifteen[0].innerText);
+    }
+    else {
+      ("");
+    }});
+
+$(document).ready(function () {
+    if (localStorage["saveSixteen"] !== null && localStorage["saveSixteen"] !== undefined) {
+      var calenderEventSixteen = $("<p>" + localStorage["saveSixteen"] + "</p>");
+      $("#sixteen").append(calenderEventSixteen[0].innerText);
+    }
+    else {
+      ("");
+    }});
